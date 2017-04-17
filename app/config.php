299eac0566039dc66673db372669c3709f7949cd
burhan <burhan.yilmaaz@gmail.com>
2017-04-17 17:35:47 +0300

    s3

---
+++ app/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+return[
+	's3'=>[
+		'key'=>'AKIAILB6GMEKDGIP5BWQ',
+		'secret'=>'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',
+		'bucket'=>'blutrondepo',
+		'region'=>'mumbai',
+		'signature' => 'v4'
+	
+	]
+]
+
+
+?>
---
+++ upload.php
@@ -0,0 +1,47 @@
+<?php
+use Aws\S3\Exception\S3Exception;
+require 'app/start.php';
+if (isset($_FILES['file'])) {
+	$file=$_FILES['file'];
+
+	$name=$file['name'];
+	$temp_name=$file['tmp_name'];
+
+	$extension=explode('.', $name);
+	$extension=strtolower(end($extension));
+	#var_dump($extension);
+
+	$key=sha1(uniqid());
+	$tmp_file_name="{$key}.{$extension}";
+	$tmp_file_path="files/{$tmp_file_name}";
+	#var_dump($tmp_file_name);
+
+	move_uploaded_file($temp_name,$tmp_file_path);
+
+	try {
+		$s3->putObject([
+			'Bucket'=>$config['s3']['bucket'],
+			'Key'=>"uploads/{$name}",
+       		'Body'   => fopen($tmp_file_path, 'rb'),
+        	'ACL'    => 'public-read'
+			]);
+
+		unlink($tmp_file_path);
+
+	} catch (S3Exception $e) {
+		echo $e;
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload</title>
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+	<input type="file" name="file">
+	<input type="submit" value="Upload">
+</form>
+</body>
+</html>
